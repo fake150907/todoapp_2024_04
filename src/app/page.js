f8a69c2e1c2724cb9cc239b79c04849931deda9e
fake150907 <yungayeon223@gmail.com>
2024-04-02 11:27:39 +0900

    MUI Appbar 컴포넌트

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,22 @@
 'use client';
+
 import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { Button, Box, AppBar, Toolbar } from '@mui/material';
 import theme from './theme';
+import { MdDeleteForever } from 'react-icons/md';
 
 export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Button variant="contained">버튼</Button>
+        <AppBar position="static">
+          <Toolbar className="tw-justify-center">
+            <a href="/" className="tw-font-bold">
+              NOTE!
+            </a>
+          </Toolbar>
+        </AppBar>
       </ThemeProvider>
     </>
   );
